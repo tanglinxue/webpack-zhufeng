ce5e7a8cd412a15781669c180ed8adea59b93f59
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2023-03-23 15:53:02 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-// let str = require('./a.js')
-require('./index.css')
-// require('./index.less')
-// console.log(str)
+console.log('home')
 
-// let fn = ()=>{
-//   console.log('logWebpack')
-// }
-// fn()
-
-
-// class A{
-//   a = 5;
-// }
-
-// let a = new A()
-// console.log(a.a)
-
-// import logo from './logo.png'
-// let image = new Image()
-// image.src = logo
-// document.body.appendChild(image)
-
-// console.log($)
+class Log{
+  constructor(){
+    console.lo('出错了')
+  }
+}
+let log = new Log()
---
+++ src1/index.js
@@ -0,0 +1,24 @@
+// let str = require('./a.js')
+require('./index.css')
+// require('./index.less')
+// console.log(str)
+
+// let fn = ()=>{
+//   console.log('logWebpack')
+// }
+// fn()
+
+
+// class A{
+//   a = 5;
+// }
+
+// let a = new A()
+// console.log(a.a)
+
+// import logo from './logo.png'
+// let image = new Image()
+// image.src = logo
+// document.body.appendChild(image)
+
+// console.log($)
